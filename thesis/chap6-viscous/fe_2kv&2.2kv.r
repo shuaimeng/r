128f7a3bda5da003d2b6311911005b914a0bf139
shuaimeng <kevindreamfly@gmail.com>
2017-05-16 17:40:27 +0800

    修改部分粘性液体的coding

---
+++ thesis/chap6-viscous/fe_2kv&2.2kv.r
@@ -0,0 +1,89 @@
+dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
+library(rJava)
+
+setwd("/Users/mengmengjiang/all datas/viscous")
+
+library(xlsx)
+
+# liquid1下的Q-V组合
+# 查看tfor和tp
+
+k1<-read.xlsx("liquid1.xlsx",sheetName="2.2kv-18",header=TRUE)
+k2<-read.xlsx("liquid1.xlsx",sheetName="2.2kv-180",header=TRUE)
+k3<-read.xlsx("liquid2.xlsx",sheetName="2.2kv-18",header=TRUE)
+k4<-read.xlsx("liquid2.xlsx",sheetName="2.2kv-90",header=TRUE)
+k5<-read.xlsx("liquid2.xlsx",sheetName="2.2kv-180",header=TRUE)
+
+# errorbar
+
+error.bar <- function(x, y, upper, coll,lower=upper, length=0.05,...){
+if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+stop("vectors must be same length")
+arrows(x,y+upper, x, y-lower,col=coll, angle=90, code=3, length=length, ...)
+}
+
+#
+yan<-rainbow(5)
+pcc<-c(0,1,2,5,22)
+yan[2]<-c("yellow3")
+
+# plot figures
+
+par(fig=c(0,1,0,1),new=T)
+
+plot(k1$fv,k1$feeva, col=0,xlab = expression(italic(f["v"]) (Hz)),
+ylab = expression(italic(f["e"])(Hz)), mgp=c(1.1, 0, 0),tck=0.02,
+main = "", xlim = c(0,800),ylim=c(0,80))
+
+mtext("2.2kv-fe",3,line=0,font=2,cex=1.1)
+
+# lines and error
+
+lines(k1$fv,k1$feeva,col=yan[1],lwd=1.5,lty=2,type="b",pch=pcc[1],cex=0.8)
+lines(k2$fv,k2$feeva,col=yan[2],lwd=1.5,lty=2,type="b",pch=pcc[2],cex=0.8)
+lines(k3$fv,k3$feeva,col=yan[3],lwd=1.5,lty=2,type="b",pch=pcc[3],cex=0.8)
+lines(k4$fv,k4$feeva,col=yan[4],lwd=1.5,lty=2,type="b",pch=pcc[4],cex=0.8)
+lines(k5$fv,k5$feeva,col=yan[5],lwd=1.5,lty=2,type="b",pch=pcc[5],cex=0.8)
+lines(k6$fv,k6$feeva,col=yan[6],lwd=1.5,lty=2,type="b",pch=pcc[6],cex=0.8)
+
+error.bar(k1$fv,k1$feeva,k1$stdfe/2,col=yan[1])
+error.bar(k2$fv,k2$feeva,k2$stdfe/2,col=yan[2])
+error.bar(k3$fv,k3$feeva,k3$stdfe/2,col=yan[3])
+error.bar(k4$fv,k4$feeva,k4$stdfe/2,col=yan[4])
+error.bar(k5$fv,k5$feeva,k5$stdfe/2,col=yan[5])
+error.bar(k6$fv,k6$feeva,k6$stdfe/2,col=yan[6])
+
+leg<-c("liquid1-18nl/min","liquid1-180nl/min","liquid2-18nl/min","liquid2-90nl/min",
+"liquid2-180nl/min")
+
+legend("topright",legend=leg,col=yan,pch=pcc,lwd=1.5,lty=2,inset=.02,bty="n",cex=0.8)
+
+par(fig=c(0.2,0.99,0.2,0.99),new=T)
+
+plot(k1$fv,k1$feeva, bty="n",col=0,xlab = expression(italic(f["v"]) (Hz)),
+ylab = expression(italic(f["e"])(Hz)), mgp=c(1.1, 0, 0),tck=0.02,
+main = "", xlim = c(0,800),ylim=c(0,60))
+
+
+
+# lines and error
+
+q1<-read.xlsx("liquid1.xlsx",sheetName="2kv-18",header=TRUE)
+q2<-read.xlsx("liquid1.xlsx",sheetName="2kv-180",header=TRUE)
+q3<-read.xlsx("liquid2.xlsx",sheetName="2kv-18",header=TRUE)
+q4<-read.xlsx("liquid2.xlsx",sheetName="2kv-90",header=TRUE)
+q5<-read.xlsx("liquid2.xlsx",sheetName="2kv-180",header=TRUE)
+
+lines(q1$fv,q1$feeva,col=yan[1],lwd=1.5,lty=2,type="b",pch=pcc[1],cex=0.8)
+lines(q2$fv,q2$feeva,col=yan[2],lwd=1.5,lty=2,type="b",pch=pcc[2],cex=0.8)
+lines(q3$fv,q3$feeva,col=yan[3],lwd=1.5,lty=2,type="b",pch=pcc[3],cex=0.8)
+lines(q4$fv,q4$feeva,col=yan[4],lwd=1.5,lty=2,type="b",pch=pcc[4],cex=0.8)
+lines(q5$fv,q5$feeva,col=yan[5],lwd=1.5,lty=2,type="b",pch=pcc[5],cex=0.8)
+
+error.bar(q1$fv,q1$feeva,q1$stdfe/2,col=yan[1])
+error.bar(q2$fv,q2$feeva,q2$stdfe/2,col=yan[2])
+error.bar(q3$fv,q3$feeva,q3$stdfe/2,col=yan[3])
+error.bar(q4$fv,q4$feeva,q4$stdfe/2,col=yan[4])
+error.bar(q5$fv,q5$feeva,q5$stdfe/2,col=yan[5])
+
+text(400,30,"2kv-fe",col="black",font=2)
---
+++ thesis/chap6-viscous/fvc.r
@@ -0,0 +1,93 @@
+dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
+library(rJava)
+
+setwd("/Users/mengmengjiang/all datas/viscous")
+
+library(xlsx)
+
+# liquid1下，2kv-18；2kv-180；2.1kv-18;2.1kv-180;2.2kv-18;2.2kv-180;
+
+
+#fvc1<-c(500,,200,500,200,500)
+
+#fpmax1<-c(500,533,700,700,700,700)
+
+# liquid12下，2kv-18,2kv-90,2kv-180;2.1kv-18;2.1kv-90;2.1kv-180;
+# 2.2kv-18;2.2kv-90;
+
+#fvc2<-c(200,50,,100,50,50,50,50)
+#fpmax2<-c(500,50,10,500,200,50,700,500)
+
+# liquid1,2kv
+
+fvc2kv<-c(500,NA,NA)
+fp2kv<-c(700,NA,NA)
+
+# liquid11,2.1kv
+fvc21kv<-c(200,500,500)
+fp21kv<-c(700,700,700)
+
+# liquid1,2.2kv
+fvc22kv<-c(200,500,500)
+fp22kv<-c(700,700,700)
+
+# liquid2-2kv
+vc2kv<-c(200,50,NA)
+p2kv<-c(500,50,10)
+
+# liquid2-2.1kv
+vc21kv<-c(100,50,50)
+p21kv<-c(500,200,50)
+
+# liquid2-2.2kv
+vc22kv<-c(50,50,NA)
+p22kv<-c(700,500,NA)
+
+
+q<-c(18,90,180)
+par(mfrow = c(2,1), mar = c(2,2.2,1.5,2), oma = c(1,1,1,1))
+
+layout(matrix(c(1,2),2,1,byrow=TRUE))
+
+yan<-rainbow(6)
+yan[2]<-c("yellow3")
+pcc<-c(0,1,2,3,5,22,23)
+
+# plot fvc
+plot(q,fvc2kv,xlab = expression(italic(Q) (nl/min)),
+ylab = expression(italic(f["vc"])(Hz)), mgp=c(1.1, 0, 0),tck=0.02,
+main = "", xlim = c(0,200),ylim=c(0,600))
+
+mtext("fvc",3,line=0,font=2,cex=1.1)
+
+lines(q,fvc2kv,col=yan[1],pch=pcc[1],type="b",lwd=1.5,lty=2)
+lines(q,fvc21kv,col=yan[2],pch=pcc[2],type="b",lwd=1.5,lty=2)
+lines(q,fvc22kv,col=yan[3],pch=pcc[3],type="b",lwd=1.5,lty=2)
+lines(q,vc2kv,col=yan[4],pch=pcc[4],type="b",lwd=1.5,lty=2)
+lines(q,vc21kv,col=yan[5],pch=pcc[5],type="b",lwd=1.5,lty=2)
+lines(q,vc22kv,col=yan[6],pch=pcc[6],type="b",lwd=1.5,lty=2)
+
+leg<-c("liquid1-2kv","liquid1-2.1kv","liquid1-2.2kv","liquid2-2kv",
+"liquid2-2.1kv","liqudi2-2.2kv")
+
+legend("topright",legend=leg,col=yan,pch=pcc,lwd=1.5,lty=2,cex=0.8,inset=.06,bty="n")
+
+# plot2 fpmax2
+
+plot(q,fvc2kv,xlab = expression(italic(Q) (nl/min)),
+ylab = expression(italic(f["pmax"])(Hz)), mgp=c(1.1, 0, 0),tck=0.02,
+main = "", xlim = c(0,200),ylim=c(0,800))
+
+mtext("fpmax",3,line=0,font=2,cex=1.1)
+
+lines(q,fp2kv,col=yan[1],pch=pcc[1],type="b",lwd=1.5,lty=2)
+lines(q,fp21kv,col=yan[2],pch=pcc[2],type="b",lwd=1.5,lty=2)
+lines(q,fp22kv,col=yan[3],pch=pcc[3],type="b",lwd=1.5,lty=2)
+lines(q,p2kv,col=yan[4],pch=pcc[4],type="b",lwd=1.5,lty=2)
+lines(q,p21kv,col=yan[5],pch=pcc[5],type="b",lwd=1.5,lty=2)
+lines(q,p22kv,col=yan[6],pch=pcc[6],type="b",lwd=1.5,lty=2)
+
+leg<-c("liquid1-2kv","liquid1-2.1kv","liquid1-2.2kv","liquid2-2kv",
+"liquid2-2.1kv","liqudi2-2.2kv")
+
+legend("topright",legend=leg,col=yan,pch=pcc,lwd=1.5,lty=2,inset=.06,cex=0.8,bty="n")
