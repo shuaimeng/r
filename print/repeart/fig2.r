33e3263dad55cb728c18e0dc9344cfb8bcf173c3
shuaimeng <kevindreamfly@gmail.com>
2017-05-30 21:52:19 +0800

    添加消耗时间的斜率拟合比较

--- print/repeart/fig2.r
+++ print/repeart/fig2.r
@@ -21,6 +21,7 @@ pcc<-c(0,1,2,5)
 
 
 # 比较转换之后的液滴的大小
+par(fig=c(0,1,0,1),new=TRUE)
 
 plot(k1$no,k1$dp, col=0,xlab = expression(italic(Number)),
           ylab = expression(italic(t["rp"])(ms)), mgp=c(1.1, 0, 0),tck=0.02,
@@ -34,7 +35,45 @@ lines(k3$no,k3$trp,col=yan[3],pch=pcc[3],lwd=1.5,lty=2,type="b")
 lines(k4$no,k4$trp,col=yan[4],pch=pcc[4],lwd=1.5,lty=2,type="b")
 
 
+
+
+
 leg<-c("600Hz","1KHz","1.5KHz","3KHz")
 
-legend("bottomright",legend=leg,col=yan,pch=pcc,lty=2,lwd=1.5,inset=.02,
+legend("topleft",legend=leg,col=yan,pch=pcc,lty=2,lwd=1.5,inset=.02,
 bty="n",cex=1)
+
+# plot 2,斜率拟合
+
+par(fig=c(0.35,0.99,0.1,0.5),new=TRUE)
+
+plot(k1$no,k1$dp,col=0,bty="n",xlab = expression(italic(Frequency)(KHz)),
+          ylab = expression(italic(slope)), mgp=c(1.1, 0, 0),tck=0.02,
+               main = "",xlim = c(1,3),ylim=c(0,1.2))
+
+z1<-lm(k1$trp~k1$no)
+z2<-lm(k2$trp~k2$no)
+z3<-lm(k3$trp~k3$no)
+z4<-lm(k4$trp~k4$no)
+
+kv1<-z1$coefficients[2]
+kv2<-z2$coefficients[2]
+kv3<-z3$coefficients[2]
+kv4<-z4$coefficients[2]
+
+kv<-c(kv2,kv3,kv4)
+x<-c(1,1.5,3)
+
+b2<-1
+b3<-1/1.5
+b4<-1/3
+
+b<-c(b2,b3,b4)
+
+lines(x,kv,col="red",lwd=1.5,lty=2,pch=0,type="b")
+lines(x,b,col="blue",lwd=1.5,lty=2,pch=1,type="b")
+
+leg2<-c("real","fitting")
+
+legend("topright",legend=leg2,lwd=1.5,lty=2,pch=c(0,1),col=c("red","blue"),
+inset=.02,bty="n",cex=1)
