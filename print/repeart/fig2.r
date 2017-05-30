0099ed55e1ee01f97b82f73fb5387ccfc6e3c099
shuaimeng <kevindreamfly@gmail.com>
2017-05-30 19:58:48 +0800

    update:消耗时间

---
+++ print/repeart/fig2.r
@@ -0,0 +1,40 @@
+dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
+library(rJava)
+
+setwd("/Users/mengmengjiang/all datas/print")
+
+library(xlsx)
+
+# reading repeart
+
+#k1<-read.xlsx("repert.xlsx",sheetName="600",header=TRUE)
+k1<-read.xlsx("repert.xlsx",sheetName="600",header=TRUE)
+
+k2<-read.xlsx("repert.xlsx",sheetName="1k",header=TRUE)
+k3<-read.xlsx("repert.xlsx",sheetName="1.5k",header=TRUE)
+k4<-read.xlsx("repert.xlsx",sheetName="3k",header=TRUE)
+
+# color setting
+
+yan<-c("red","blue","black","green3")
+pcc<-c(0,1,2,5)
+
+
+# 比较转换之后的液滴的大小
+
+plot(k1$no,k1$dp, col=0,xlab = expression(italic(Number)),
+          ylab = expression(italic(t["rp"])(ms)), mgp=c(1.1, 0, 0),tck=0.02,
+               main = "", xlim = c(0,300),ylim=c(0,100))
+
+mtext("Cost time",3,line=0.2,font=2,cex=1.2)
+
+lines(k1$no,k1$trp,col=yan[1],pch=pcc[1],lwd=1.5,lty=2,type="b")
+lines(k2$no,k2$trp,col=yan[2],pch=pcc[2],lwd=1.5,lty=2,type="b")
+lines(k3$no,k3$trp,col=yan[3],pch=pcc[3],lwd=1.5,lty=2,type="b")
+lines(k4$no,k4$trp,col=yan[4],pch=pcc[4],lwd=1.5,lty=2,type="b")
+
+
+leg<-c("600Hz","1KHz","1.5KHz","3KHz")
+
+legend("bottomright",legend=leg,col=yan,pch=pcc,lty=2,lwd=1.5,inset=.02,
+bty="n",cex=1)
