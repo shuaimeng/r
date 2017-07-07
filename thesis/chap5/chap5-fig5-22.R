daee18b70253228d4df96a2fbc526ce655a4f1b6
shuaimeng <kevindreamfly@gmail.com>
2017-07-07 15:53:57 +0800

    更新：第五章fig5-22

--- thesis/chap5/chap5-fig5-22.R
+++ thesis/chap5/chap5-fig5-22.R
@@ -1,114 +1,129 @@
 dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/lib/server/libjvm.dylib')
 
-setwd("/Users/mengmengjiang/all datas/chap5")
+setwd("/Users/mengmengjiang/all datas/voltage")
+
+#对单个内容进行画图分析，尝试记录单个内容的pdf图片格式
+#文件名：voltage_stable , OV电压与BV电压之差，即stable 稳定电压与三种液体之间随流量、极间距离、针头直径变化的影响
+
 library(xlsx)
 
-#针头为25G
-n1<-read.xlsx("he-25g.xlsx",sheetName="2kv18",header=TRUE)
-n2<-read.xlsx("he-25g.xlsx",sheetName="2kv54",header=TRUE)
-n3<-read.xlsx("he-25g.xlsx",sheetName="2kv180",header=TRUE)
+#读取v~Q的数据
+eq<-read.xlsx("voltage.xls",sheetName="ethanol_q",header=TRUE)
+aq<-read.xlsx("voltage.xls",sheetName="acetone_q",header=TRUE)
+iq<-read.xlsx("voltage.xls",sheetName="iso_q",header=TRUE)
+
+###布局###
+par(mar=c(2,2.2,0.2,1),oma=c(2,2,2,2))
+layout(matrix(c(1,1,2,3), 2, 2,byrow = TRUE))
+
+plot(eq$f, eq$va,  col=0, xaxs="i", xlim=c(-0.002, 0.04), ylim=c(1, 3),
+     xlab=expression(italic(Q)(nl/min)),mgp=c(1, 0, 0),tck=0.01,cex.lab=1.1,
+     ylab=expression(italic(V)(kv)))
+
+mtext("Flowrate",col="black",3,line=-1,font=2,cex=0.9)
+
+###error bar####
+error.bar <- function(x, y, upper, coll,lower=upper, length=0.05,...){
+  if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+    stop("vectors must be same length")
+  arrows(x,y+upper, x, y-lower,col=coll, angle=90, code=3, length=length, ...)
+}
+
+###lines##
+yan<-c("red","red","blue","blue","green3","green3")
+pch<-c(1,2,1,2,1,2)
+
+lines(eq$f,eq$vaeva,col=yan[1],pch=1,lwd=2,lty=4,type="b")
+lines(eq$f,eq$vbeva,col=yan[2],pch=2,lwd=2,lty=4,type="b")
+lines(aq$f,aq$vaeva,col=yan[3],pch=1,lwd=2,lty=4,type="b")
+lines(aq$f,aq$vbeva,col=yan[4],pch=2,lwd=2,lty=4,type="b")
+lines(iq$f,iq$vaeva,col=yan[5],pch=1,lwd=2,lty=4,type="b")
+lines(iq$f,iq$vbeva,col=yan[6],pch=2,lwd=2,lty=4,type="b")
+
+error.bar(eq$f,eq$vaeva,eq$vastd/2,col=yan[1])
+error.bar(eq$f,eq$vbeva,eq$vbstd/2,col=yan[2])
+
+error.bar(aq$f,aq$vaeva,aq$vastd/2,col=yan[3])
+error.bar(aq$f,aq$vbeva,aq$vbstd/2,col=yan[4])
+
+error.bar(iq$f,iq$vaeva,iq$vastd/2,col=yan[5])
+error.bar(iq$f,iq$vbeva,iq$vbstd/2,col=yan[6])
+
+leg<-c("ethanol-Von","ethanol-Vbr","acetone-Von","acetone-Vbr","iso-Von","iso-Vbr")
+
+legend("bottomright",legend=leg, col=yan, pch=c(1,2,1,2,1,2),bty="n",lwd=2,lty=2,inset=.01,cex=0.9)
+
+#读取v~Q的数据
+eq<-read.xlsx("voltage.xls",sheetName="ethanol_d",header=TRUE)
+aq<-read.xlsx("voltage.xls",sheetName="acetone_d",header=TRUE)
+iq<-read.xlsx("voltage.xls",sheetName="iso_d",header=TRUE)
+
+plot(eq$d, eq$va,  col=0, xaxs="i", xlim=c(0.5,4.5), ylim=c(1, 3.3),xlab=expression(italic(Distance)(mm)),mgp=c(1, 0,0),tck=0.01,cex.lab=1.1,ylab=expression(italic(V)(kv)))
+
+mtext("Distance",col="black",3,line=-1,font=2,cex=0.9)
+
+###error bar####
+error.bar <- function(x, y, upper, coll,lower=upper, length=0.05,...){
+  if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+    stop("vectors must be same length")
+  arrows(x,y+upper, x, y-lower,col=coll, angle=90, code=3, length=length, ...)
+}
+
+###lines##
+yan<-c("red","red","blue","blue","green3","green3")
+pch<-c(1,2,1,2,1,2)
+
+lines(eq$d,eq$vaeva,col=yan[1],pch=1,lwd=2,lty=4,type="b")
+lines(eq$d,eq$vbeva,col=yan[2],pch=2,lwd=2,lty=4,type="b")
+lines(aq$d,aq$vaeva,col=yan[3],pch=1,lwd=2,lty=4,type="b")
+lines(aq$d,aq$vbeva,col=yan[4],pch=2,lwd=2,lty=4,type="b")
+lines(iq$d,iq$vaeva,col=yan[5],pch=1,lwd=2,lty=4,type="b")
+lines(iq$d,iq$vbeva,col=yan[6],pch=2,lwd=2,lty=4,type="b")
+
+error.bar(eq$d,eq$vaeva,eq$vastd/2,col=yan[1])
+error.bar(eq$d,eq$vbeva,eq$vbstd/2,col=yan[2])
+
+error.bar(aq$d,aq$vaeva,aq$vastd/2,col=yan[3])
+error.bar(aq$d,aq$vbeva,aq$vbstd/2,col=yan[4])
+
+error.bar(iq$d,iq$vaeva,iq$vastd/2,col=yan[5])
+error.bar(iq$d,iq$vbeva,iq$vbstd/2,col=yan[6])
 
 
-#读取针头为30g
-n4<-read.xlsx("he-30g.xlsx",sheetName="2kv18",header=TRUE)
-n5<-read.xlsx("he-30g.xlsx",sheetName="2kv54",header=TRUE)
-n6<-read.xlsx("he-30g.xlsx",sheetName="2kv180",header=TRUE)
-
-#针头为32g
-n7<-read.xlsx("he-32g.xlsx",sheetName="2kv18",header=TRUE)
-n8<-read.xlsx("he-32g.xlsx",sheetName="2kv54",header=TRUE)
-n9<-read.xlsx("he-32g.xlsx",sheetName="2kv180",header=TRUE)
-
-#针头为34g
-n10<-read.xlsx("he-34g.xlsx",sheetName="2kv18",header=TRUE)
-n11<-read.xlsx("he-34g.xlsx",sheetName="2kv54",header=TRUE)
-n12<-read.xlsx("he-34g.xlsx",sheetName="2kv180",header=TRUE)
-
-###画图
-plot(n1$fv, n1$he_ra, xlab = expression(log(q["d"])),
-     ylab=expression(log(f["e"])),mgp=c(1.1, 0, 0),tck=0.02,
-     xlim=c(-14,-2),ylim=c(-2,8),col=0)
-
-###颜色###
-yan<-rainbow(9)
-
-####30g针头下###
-
-#1#18nl-30g##
-a<-log(0.5*18/(n4$fv*60*3.1^3))
-
-a1<-log((500/n4$fv - n4$tf)/n4$tp)
-
-#2###18nl-32g###
-b<-log(0.5*18/(n7$fv*60*2.3^3))
-b1<-log((500/n7$fv - n7$tf)/n7$tp)
-
-#3#54nl-30g##
-c<-log(0.5*54/(n5$fv*60*3.1^3))
-
-c1<-log((500/n5$fv - n5$tf)/n5$tp)
-
-#4#18nl-34G##
-d<-log(0.5*18/(n10$fv*60*1.9^3))
-
-d1<-log((500/n10$fv - n10$tf)/n10$tp)
-
-#5#54nl-32G##
-e<-log(0.5*54/(n8$fv*60*2.3^3))
-e1<-log((500/n8$fv - n8$tf)/n8$tp)
-
-#6#54nl-34G##
-f<-log(0.5*54/(n11$fv*60*1.9^3))
-
-f1<-log((500/n11$fv - n11$tf)/n11$tp)
-
-#7#180nl-30G##
-g<-log(0.5*180/(n6$fv*60*3.1^3))
-g1<-log((500/n6$fv - n6$tf)/n6$tp)
-
-#8#180nl-32G##
-h<-log(0.5*180/(n9$fv*60*2.3^3))
-h1<-log((500/n9$fv - n9$tf)/n9$tp)
-
-#9#180nl-34G##
-i<-log(0.5*180/(n12$fv*60*1.9^3))
-i1<-log((500/n12$fv - n12$tf)/n12$tp)
-
-###
-xx<-c(a,b,c,d,e,f,g,h,i)
-yy<-c(a1,b1,c1,d1,e1,f1,g1,h1,i1)
-pchc<-c(1,2,3,4,5,6,7,22,24)
-
-#画点
-points(lowess(a,a1,f=1/4,iter=3),col=yan[1],pch=1,lwd=2,lty=2,cex=0.8)
-points(lowess(b,b1,f=1/4,iter=3),col=yan[2],pch=2,lwd=2,lty=2,cex=0.8)
-points(lowess(c,c1,f=1/4,iter=3),col=yan[3],pch=3,lwd=2,lty=2,cex=0.8)
-
-points(lowess(d,d1,f=1/4,iter=3),col=yan[4],pch=4,lwd=2,lty=2,cex=0.8)
-points(lowess(e,e1,f=1/4,iter=3),col=yan[5],pch=5,lwd=2,lty=2,cex=0.8)
-points(lowess(f,f1,f=1/4,iter=3),col=yan[6],pch=6,lwd=2,lty=2,cex=0.8)
 
 
-points(lowess(g,g1,f=1/4,iter=3),col=yan[7],pch=7,lwd=2,lty=2,cex=0.8)
-points(lowess(h,h1,f=1/4,iter=3),col=yan[8],pch=22,lwd=2,lty=2,cex=0.8)
-points(lowess(i,i1,f=1/4,iter=3),col=yan[9],pch=24,lwd=2,lty=2,cex=0.8)
+#读取v~Q的数据
+eq<-read.xlsx("voltage.xls",sheetName="ethanol_r",header=TRUE)
+aq<-read.xlsx("voltage.xls",sheetName="acetone_r",header=TRUE)
+iq<-read.xlsx("voltage.xls",sheetName="iso_r",header=TRUE)
 
-##拟合
+plot(eq$r, eq$va,  col=0, xaxs="i", xlim=c(0.25,0.85), ylim=c(1, 3.3),xlab=expression(paste(italic(Nozzle)," ",italic(diameter),(mm))),mgp=c(1, 0,0),tck=0.01,cex.lab=1.1,ylab=expression(italic(V)(kv)))
 
-abline(lm(a1~a),col=yan[1],lty=4)
-abline(lm(b1~b),col=yan[2],lty=4)
-abline(lm(c1~c),col=yan[3],lty=4)
+mtext("Nozzles",col="black",3,line=-1,font=2,cex=0.9)
 
-abline(lm(d1~d),col=yan[4],lty=4)
-abline(lm(e1~e),col=yan[5],lty=4)
-abline(lm(f1~f),col=yan[6],lty=4)
+###error bar####
+error.bar <- function(x, y, upper, coll,lower=upper, length=0.05,...){
+  if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+    stop("vectors must be same length")
+  arrows(x,y+upper, x, y-lower,col=coll, angle=90, code=3, length=length, ...)
+}
 
-abline(lm(g1~g),col=yan[7],lty=4)
-abline(lm(h1~h),col=yan[8],lty=4)
-abline(lm(i1~i),col=yan[9],lty=4)
+###lines##
+yan<-c("red","red","blue","blue","green3","green3")
+pch<-c(1,2,1,2,1,2)
 
-leg<-c("18nl/min-30G","18nl/min-32G","54nl/min-30G",
-       "18nl/min-34G","54nl/min-32G",
-       "180nl/min-30G","54nl/min-34G","180nl/min-32G","180nl/min-34G")
+lines(eq$r,eq$vaeva,col=yan[1],pch=1,lwd=2,lty=4,type="b")
+lines(eq$r,eq$vbeva,col=yan[2],pch=2,lwd=2,lty=4,type="b")
+lines(aq$r,aq$vaeva,col=yan[3],pch=1,lwd=2,lty=4,type="b")
+lines(aq$r,aq$vbeva,col=yan[4],pch=2,lwd=2,lty=4,type="b")
+lines(iq$r,iq$vaeva,col=yan[5],pch=1,lwd=2,lty=4,type="b")
+lines(iq$r,iq$vbeva,col=yan[6],pch=2,lwd=2,lty=4,type="b")
 
-legend("topleft",legend=leg,col=yan,pch=pchc,bty="n",lwd=1.5,lty=2,inset=.05,cex=0.8)
+error.bar(eq$r,eq$vaeva,eq$vastd/2,col=yan[1])
+error.bar(eq$r,eq$vbeva,eq$vbstd/2,col=yan[2])
+
+error.bar(aq$r,aq$vaeva,aq$vastd/2,col=yan[3])
+error.bar(aq$r,aq$vbeva,aq$vbstd/2,col=yan[4])
+
+error.bar(iq$r,iq$vaeva,iq$vastd/2,col=yan[5])
+error.bar(iq$r,iq$vbeva,iq$vbstd/2,col=yan[6])
