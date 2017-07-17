3cf546fb88390a1add7938b041ecb5e84f7ba545
shuaimeng <kevindreamfly@gmail.com>
2017-07-17 17:09:52 +0800

    更新：第五章fig5-7

--- thesis/chap5/chap5-fig5-9.R
+++ thesis/chap5/chap5-fig5-9.R
@@ -4,6 +4,23 @@ dyn.load('/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/jre/l
 setwd("/Users/mengmengjiang/all datas/chap5")
 library(xlsx)
 
+par(mar=c(2,2.2,0.8,0.8),oma=c(2,2,2,2))
+layout(matrix(c(1,2), 2, 1,byrow = TRUE))
+
+x<-c(1,2,3,4)
+y1<-c(2.2,2.2,2.2,2)
+y2<-c(2,1.6,1.8,1.8)
+
+plot(x,y1,type="l",lwd=2,lty=2,col=0,xlab = expression(italic(Nozzles)),
+     ylab = expression(italic(Voltage-range)(kv)),mgp=c(1.1, 0, 0),tck=0.02,xaxt="n",xlim = c(0.9, 4.1),ylim=c(1.4,2.4))
+axis(1,mgp=c(1.1, 0, 0),tck=0.02,at=c(1,2,3,4),label=c("25G","30G","32G","34G"))
+points(x,y1,col="red",pch=1,lwd=2,lty=2,type="b")
+points(x,y2,col="blue",pch=2,lwd=2,lty=2,type="b")
+
+text(2,2.3,"High limit voltage",col="red",font=2)
+text(3.2,1.7,"Low limit voltage",col="blue",font=2)
+
+
 #25G
 x1<-c(2,2.2)
 y1<-c(0.8,0.8)
@@ -16,6 +33,8 @@ y3<-c(2.8,3.5)
 #34G
 x4<-c(1.8,2)
 y4<-c(2,3)
+
+
 
 plot(x1,y1, xlab = expression(italic(f["v"])(Kv)),
      ylab=expression(italic(f["pmax"](KHz))),mgp=c(1.1, 0, 0),tck=0.02,
